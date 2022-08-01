f4f2de7f99bb846f6d0887defbd7d68ed536bdb4
Travis <travisamador@gmail.com>
2022-08-01 16:57:06 -0400

    first push for country colors lab

--- country.js
+++ country.js
@@ -1,20 +1,37 @@
 class Country {
-    constructor(name, lang, greeting, colors) {
+    constructor(name, lang, greeting, colors, flagURL) {
         this.name = name;
         this.lang = lang;
         this.greeting = greeting;
         this.colors = colors;
+        this.flagURL = flagURL
     }
-}
-let usa = new Country("USA", "Murican", "Why Hello there world! Have some McDonalds", ["red", "white", "blue"]);
-let mexico = new Country("Mexico", "Spanish", "Hola mundo!", ["red", "white", "green"]);
-let algeria = new Country("Algeria", "Arabic and Berber", "Sup", ["blue", "yellow", "red"])
+    ChangeInfo(){
+        document.getElementById("CountryName").innerText = this.name;
+        document.getElementById("OfficialLanguage").innerText = this.lang;
+        document.getElementById("HelloWorld").innerText = this.greeting;
+    };
+    ChangeColors(){
+        document.getElementById("Color1").style.backgroundColor = this.colors[0];
+        document.getElementById("Color2").style.backgroundColor = this.colors[1];
+        document.getElementById("Color3").style.backgroundColor = this.colors[2];
+    }
+    ChangeFlag(){
+        document.getElementById("Flag").src = this.flagURL;
+    }
 
+}
+let usa = new Country("USA", "Murican", "Why Hello there world! Have some McDonalds", ["red", "white", "blue"], "USAFlag.png");
+let mexico = new Country("Mexico", "Spanish", "Hola mundo!", ["red", "white", "green"], "MexicoFlag.png");
+let algeria = new Country("Algeria", "Arabic and Berber", "Sup", ["blue", "yellow", "red"], "AlgeriaFlag.png");
+let chad = new Country("Chad", "Arabic and French", "Marhaban bialealam", ["blue", "yellow", "red"], "ChadFlag.png");
+let italy = new Country("Italy", "Italian", "Ciao mondo", ["green", "white", "red"], "ItalyFlag.png");
+let germany = new Country("Germany", "German", "Hallo Welt", ["black", "red", "gold"], "GermanyFlag.png");
+let yemen = new Country("Yemen", "Arabic", "Marhaban bialealam", ["red", "black", "white"], "YemenFlag.png");
 
 function SwitchCountry() {
-    let input = "USA";
+    let input = document.getElementById("CountryList").value;
     let country;
-
     if (input === "USA") {
         //set country to usa 
         country = usa;
@@ -25,5 +42,20 @@ function SwitchCountry() {
     else if (input === "Algeria") {
         country = algeria;
     }
+    else if (input === "Chad") {
+        country = chad;
+    }
+    else if (input === "Italy") {
+        country = italy;
+    }
+    else if (input === "Germany") {
+        country = germany;
+    }
+    else if (input === "Yemen") {
+        country = yemen;
+    }
+    country.ChangeInfo();
+    country.ChangeColors(); 
+    country.ChangeFlag(); 
 
 }
